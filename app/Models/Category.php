52f0eb0f1c40001f6b11786a0afba21be26ddd7f
Javier <javi.inclusion@gmail.com>
2024-12-09 21:53:28 +0100

    relaciones tablas

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+    //podemos añadir campos masivos
+    protected $guarded =[];
+    //Creamos las relaciones 1 a muchos.
+    public function posts() : HasMany{
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Comment extends Model
 {
-    //
+    use HasFactory;
+    protected $guarded=[];
+
+    /**
+     * Relación con la tabla posts
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class, 'post_id');
+    }
 }
